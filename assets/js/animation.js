89a48bf40cfa1f7e95b7191b15f3d8c1de21eebf
SoutMatin <shayakhmetov010@gmail.com>
2026-05-20 22:11:17 +0300

    added header animation

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -1,3 +1,36 @@
-document.addEventListener("DOMContentLoaded", (event) => {
-    gsap.registerPlugin(ScrollTrigger);
-})
+gsap.registerPlugin(ScrollTrigger);
+const headerAnims = document.querySelectorAll(".header-anim");
+
+document.querySelectorAll(".trigger-section").forEach((section) => {
+  ScrollTrigger.create({
+    trigger: section,
+    start: "-1px",
+    end: "bottom",
+
+    onEnter: () => {
+      headerAnims.forEach((e) => {
+        e.classList.remove("header-change");
+      });
+    },
+
+    onLeave: () => {
+      headerAnims.forEach((e) => {
+        e.classList.add("header-change");
+      });
+    },
+
+    onEnterBack: () => {
+      headerAnims.forEach((e) => {
+        e.classList.remove("header-change");
+      });
+    },
+
+    onLeaveBack: () => {
+      headerAnims.forEach((e) => {
+        e.classList.add("header-change");
+      });
+    },
+
+    markers: true,
+  });
+});
