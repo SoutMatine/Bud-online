083c79fbaa13a66599e6093dc40e98fa254d976b
SoutMatine <shayakhmetov010@gmail.com>
2026-05-22 16:55:57 +0300

    update page, styles, animations and added images

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -10,6 +10,59 @@ ScrollSmoother.create({
   ignoreMobileResize: true,
 })
 
+const header = document.querySelector("header")
+
+const mainText = new SplitType("#mainText")
+const mainSubtitle = document.querySelector("#mainSubtitle")
+
+gsap.set(mainSubtitle, { y: -25, opacity: 0 })
+gsap.set(header, { opacity: 0 })
+
+setTimeout(() => {
+  gsap.to('.char', {
+    y: 0,
+    stagger: 0.01,
+    delay: 0.1,
+    duration: 0.1
+  })
+
+  gsap.to(mainSubtitle,
+    { y: 0, opacity: 1, duration: 0.5, delay: 1 }
+  )
+
+  gsap.to(header,
+    { opacity: 1, duration: 0.5, delay: 1.5 }
+  )
+
+}, 500);
+
+
+
+
+
+
+
+const tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.main-screen',
+    start: 'top top',
+    end: '250svh', // длина скролла = высоте hero
+    pin: true,
+    scrub: 1,
+    anticipatePin: 1
+  }
+});
+
+// Анимация перекрытия
+tl.to('.completed-works', {
+  y: '-80svh', // или '-100svh', поднимаем контейнер вверх
+  ease: 'none',
+  duration: 3
+});
+
+
+
+/* ======================= Header Animation ======================= */
 
 const headerAnims = document.querySelectorAll(".header-anim");
 
@@ -47,66 +100,26 @@ document.querySelectorAll(".dark-section").forEach((section) => {
   });
 });
 
+/* ======================= Header Animation ======================= */
+
+
 
 
 /* ======================= Completed Works ======================= */
-
-function isFastElement(index) {
-  // Позиция в ряду (1-based)
-  const position = index + 1;
-  // Чётные позиции (2, 4, 6, 8...) - быстрые
-  return position % 2 === 0;
-}
-
-// Получаем все секции с элементами
-const sections = document.querySelectorAll('.different-speed');
-
-sections.forEach((section, sectionIndex) => {
-  // Находим все элементы в текущей секции
-
-  const completedWorks = document.querySelectorAll('.completed-work')
-
-  // Помечаем быстрые элементы визуально
-  completedWorks.forEach((item, idx) => {
-    if (isFastElement(idx)) {
-      item.classList.add('fast');
-    }
-  });
-
-  // Настраиваем анимацию для каждого элемента
-  completedWorks.forEach((item, idx) => {
-    // Определяем скорость: быстрые элементы (чётные) - в 1.5 раза быстрее
-    const speed = isFastElement(idx) ? 1.5 : 1;
-
-    // Создаём анимацию движения
-    gsap.to(item, {
-      y: 0 * speed,
-      ease: "none",
-      scrollTrigger: {
-        trigger: section,
-        start: "top bottom",
-        end: "bottom top",
-        scrub: 1,
-        invalidateOnRefresh: true
-      }
-    });
-  });
-});
-
-
 let itemLeft = gsap.utils.toArray('.grid-line .left-item')
 let itemRight = gsap.utils.toArray('.grid-line .right-item')
 
 let item = gsap.utils.toArray('.grid-line')
 item.forEach(item => {
-  gsap.fromTo(item, { y: 300, opacity: 0.5 }, {
-    opacity: 1, y: 0,
+  gsap.fromTo(item, { y: 300, opacity: 0.5, scale: 0.9 }, {
+    opacity: 1, y: 0, scale: 1,
     scrollTrigger: {
       trigger: item,
-      start: 'top bottom',
-      end: 'top top+=400',
+      start: 'top bottom+=700',
+      end: 'top top+=1200',
       scrub: 1,
       once: true,
+
     }
   })
 })
@@ -116,11 +129,10 @@ itemLeft.forEach(item => {
     opacity: 1, x: 0,
     scrollTrigger: {
       trigger: item,
-      start: 'top bottom',
-      end: 'top top+=400',
+      start: 'top bottom+=700',
+      end: 'top top+=1100',
       scrub: true,
       once: true,
-
     }
   }
   )
@@ -131,10 +143,10 @@ itemRight.forEach(item => {
     opacity: 1, x: 0,
     scrollTrigger: {
       trigger: item,
-      start: 'top bottom',
-      end: 'top top+=400',
+      start: 'top bottom+=700',
+      end: 'top top+=1100',
       scrub: true,
-      once: true
+      once: true,
     }
   }
   )
